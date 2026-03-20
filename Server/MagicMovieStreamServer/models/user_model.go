64a3c55baa912b040c965fa642470ef0c7095356
saroj580 <chaudharysaroj819@gmail.com>
2026-03-21 00:17:22 +0530

    added more user reference and login model

--- Server/MagicMovieStreamServer/models/user_model.go
+++ Server/MagicMovieStreamServer/models/user_model.go
@@ -20,3 +20,17 @@ type User struct{
 	RefreshToken string `json:"refresh_token" bson:"refresh_token"`
 	FavouriteGenres []Genre `json:"favourite_genres" bson:"favourite_genres" validate:"required,dive"`
 }
+
+type UserLogin struct{
+	Email string `json:"email" bson:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type UaserResponse struct{
+	UserID string `json:"user_id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	Role string `json:"role"`
+	FavouriteGenre []Genre `json:"favourite_genres"`
+}
