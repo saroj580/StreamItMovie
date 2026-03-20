2a6c2b0aaef50626f0c2cbe6f687929191fcc3cb
saroj580 <chaudharysaroj819@gmail.com>
2026-03-21 00:46:02 +0530

    added the token utils

--- Server/MagicMovieStreamServer/models/user_model.go
+++ Server/MagicMovieStreamServer/models/user_model.go
@@ -15,7 +15,7 @@ type User struct{
 	Password string `json:"password" bson:"password" validate:"required,min=6"`
 	Role string `json:"role" bson:"role" validate:"oneof=ADMIN USER"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time `json:"updated_ad" bson:"updated_ad"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 	Token string `json:"token" bson:"token"`
 	RefreshToken string `json:"refresh_token" bson:"refresh_token"`
 	FavouriteGenres []Genre `json:"favourite_genres" bson:"favourite_genres" validate:"required,dive"`
